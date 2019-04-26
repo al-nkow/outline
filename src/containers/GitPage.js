cf4f4d764f36df8c0cef4a1b9835034419fb4b3f
Aleksey <al.nkow@gmail.com>
2019-04-26 16:18:39 +0300

    git

--- src/containers/GitPage.js
+++ src/containers/GitPage.js
@@ -14,6 +14,8 @@ class GitPage extends PureComponent {
           <Code className="mr">git chery-pick</Code> - забрать комиты из одной ветки в другую<br/>
           <Code className="mr">git chery-pick 3ea6207fe84</Code> - взять комит с индентификатором <b>3ea6207fe84</b> из той
           ветки, в которой он был сделан, и изменения этого коммита будут вставлены в текущую ветку как новый коммит<br/>
+          <Code className="mr">git branch -d <b>имя_ветки</b></Code> - удалить ветку из локального Git-репозитория<br/>
+          <Code className="mr">git push origin --delete <b>имя_ветки</b></Code> - удалить ветку из удаленного Git-репозитория<br/>
         </p>
       </div>
     );
